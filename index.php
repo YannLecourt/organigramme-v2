3a194446c174574c93751fc9f7d8071e0e26fbdb
YannLecourt <yann.lecourt@gmail.com>
2015-08-29 21:48:15 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang= "en">
+<head>
+	<title>French Renewal Team</title>
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<link rel="stylesheet" href="style.css" 
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+	<div class="jumbotron">
+	<div style="background-color:black; color:white; text-align:center; padding: 5px;">
+	<h1> Organizational Chart </h1>
+	</div>
+	<div class="row">
+	<div class="col-sm-6">
+				
+	<!--table-->	
+	
+	<tr>
+			<td style='width: 30%;'><img class = 'mgr' src='images/fabrice.jpg'>
+			</td>
+			<td>
+				<p class='description'></p> Fabrice Gabaston - <strong> Manager - </strong> <span class="blue"> 02 XX XX XX XX </span><br></br>
+				Digital Card <a href="http://www.ibm.com/connect/ibm/fr/fr/resources/gabaston_fabrice/" target="_blank"> Here </a>
+				<p><a href="https://twitter.com/fgabaston" target="_blank"> <img src="images/twitter-icon.png" alt="twitter" style="width:42px;height:42px;border:0"></a>
+				<a href="https://ie.linkedin.com/pub/fabrice-gabaston/8/458/88a" target="_blank"> <img src="images/LinkedIn.png" alt="LinkedIn" style="width:42px;height:42px;border:0"></a></p>
+				</td>
+	</tr>
+	</div>
+	
+	<div class="col-sm-6">
+	
+		
+	<tr>
+			<td style='width: 30%;'><img class = 'mgr' src='images/greg.jpg'>
+			</td>
+			<td>		
+				<p class='description'></p> Gregori Lafont - <strong> Manager - </strong> <span class="blue"> 02 XX XX XX XX </span><br></br>
+				Digital Card <a href="http://www.ibm.com/connect/ibm/fr/fr/resources/gregori_lafont/" target="_blank"> Here </a>
+				<p><a href="https://twitter.com/greglafont" target="_blank"> <img src="images/twitter-icon.png" alt="twitter" style="width:42px;height:42px;border:0"></a>
+				<a href="https://www.linkedin.com/in/greglafont" target="_blank"> <img src="images/LinkedIn.png" alt="LinkedIn" style="width:42px;height:42px;border:0"></a></p>
+				</td>
+
+				
+		</tr>
+			
+		</div>
+		</div>
+		</div>
+						
+		<!--table-->		
+	
+	<!--table-->
+	
+	<div class="col-sm-6">
+			<tr>
+				
+			<td style='width: 30%;'><img class = 'newappIcon' src='images/yann.jpg'>
+			</td>
+			<td>
+
+				<p class='description'></p> Yann Lecourt - Public Sector - <span class="blue"> 02 38 55 79 51</span><br></br>
+				Digital Card <a href="http://www.ibm.com/connect/ibm/fr/fr/resources/yann_lecourt/" target="_blank"> Here </a>
+				<p><a href="https://twitter.com/yannlecourt" target="_blank"> <img src="images/twitter-icon.png" alt="twitter" style="width:42px;height:42px;border:0"></a>
+				<a href="http://lnkd.in/bfnCah" target="_blank"> <img src="images/LinkedIn.png" alt="LinkedIn" style="width:42px;height:42px;border:0"></a></p>
+				
+				
+				</td>
+		</tr>
+		
+		
+		<tr>
+			<td style='width: 30%;'><img class = 'newappIcon' src='images/thierry.jpg'>
+			</td>
+			<td>
+				
+			<p class='description'></p> Thierry Menanteau - Distribution - <span class="blue"> 02 38 55 XX XX</span><br></br>
+				Digital Card <a href="http://www.ibm.com/connect/ibm/fr/fr/resources/thierry_menanteau/" target="_blank"> Here </a>
+				<p><a href="https://twitter.com/Thierrymen" target="_blank"> <img src="images/twitter-icon.png" alt="twitter" style="width:42px;height:42px;border:0"></a>
+				<a href="http://ie.linkedin.com/pub/thierry-menanteau/1/886/591" target="_blank"> <img src="images/LinkedIn.png" alt="LinkedIn" style="width:42px;height:42px;border:0"></a></p>
+				
+				</td>
+		</tr>
+		</div>
+		
+		<div class="col-sm-6">
+				<tr>
+			<td style='width: 30%;'><img class = 'newappIcon' src='images/eric.jpg'>
+			</td>
+			<td>
+				
+
+			<p class='description'></p> Eric Roselier - Finance - <span class="blue"> 02 38 55 XX XX </span><br></br>
+				Digital Card <a href="http://www.ibm.com/connect/ibm/fr/fr/resources/eric_roselier/" target="_blank"> Here </a>
+				<p><a href="https://twitter.com/EricRoselier" target="_blank"> <img src="images/twitter-icon.png" alt="twitter" style="width:42px;height:42px;border:0"></a>
+				<a href="https://ie.linkedin.com/pub/eric-roselier/38/252/283" target="_blank"> <img src="images/LinkedIn.png" alt="LinkedIn" style="width:42px;height:42px;border:0"></a></p>
+				
+			</td>
+		</tr>
+		
+		<tr>
+			<td style='width: 30%;'><img class = 'newappIcon' src='images/mathieu.jpg'>
+			</td>
+			<td>
+				
+
+				<p class='description'></p> Mathieu Boucher <br></br> <span class="blue"> Distribution </span> Digital Card <a
+				href="http://www.ibm.com/connect/ibm/fr/fr/resources/mathieu_boucher/"> Click Here! </a>
+				</td>
+		</tr>
+		</div>
+		
+		
+	<!--/table-->
+	
+	<div id="footer">
+	IBM Digital Sales Center - Dublin
+	</div>
+</body>
+</html>
